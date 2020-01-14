4217e8c306c9aaf3636d6a4771fb3c9f9d1e76d0
Opilane TTHK <opilane@tthk.ee>
2020-01-14 10:50:02 +0200

    m

--- Vytas_Lab3/Form1.Designer.cs
+++ Vytas_Lab3/Form1.Designer.cs
@@ -30,8 +30,6 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,7 +37,7 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(13, 13);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(775, 425);
+            this.pictureBox1.Size = new System.Drawing.Size(400, 700);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
@@ -48,42 +46,19 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Silver;
-            this.label1.Location = new System.Drawing.Point(119, 363);
+            this.label1.Location = new System.Drawing.Point(181, 13);
             this.label1.Name = "label1";
             this.label1.Padding = new System.Windows.Forms.Padding(15);
             this.label1.Size = new System.Drawing.Size(56, 43);
             this.label1.TabIndex = 1;
             this.label1.Text = "Вид";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Silver;
-            this.label2.Location = new System.Drawing.Point(347, 363);
-            this.label2.Name = "label2";
-            this.label2.Padding = new System.Windows.Forms.Padding(15);
-            this.label2.Size = new System.Drawing.Size(74, 43);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Форма";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Silver;
-            this.label3.Location = new System.Drawing.Point(569, 363);
-            this.label3.Name = "label3";
-            this.label3.Padding = new System.Windows.Forms.Padding(15);
-            this.label3.Size = new System.Drawing.Size(103, 43);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Информация";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(425, 727);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
@@ -98,8 +73,6 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
     }
 }
 
--- Vytas_Lab3/Form1.cs
+++ Vytas_Lab3/Form1.cs
@@ -1,171 +1,100 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Vytas_Lab3
 {
     public partial class Form1 : Form
     {
-        bool rectangleClicked, circleClicked, squareClicked, swapped = false;
-        int x1, x2, x3, y1, y2, y3, X, Y, dX, dY, LastClicked = 0;
+        bool circleClicked = false;
+        int score, x2, LastClicked = 0;
+        int speed = 2;
 
         private void PictureBox1_Paint(object sender, PaintEventArgs e)
         {
             e.Graphics.FillEllipse(Brushes.Red, circle);
-            e.Graphics.FillRectangle(Brushes.Blue, square);
             e.Graphics.FillRectangle(Brushes.Yellow, rectangle);
         }
 
-        Rectangle circle = new Rectangle(220, 10, 150, 150);
-        Rectangle square = new Rectangle(380, 10, 150, 150);
-        Rectangle rectangle = new Rectangle(10, 10, 200, 100);
+        Rectangle circle = new Rectangle(175, 600, 50, 50);
+        Rectangle square = new Rectangle(380, 10, 75, 75);
+        Rectangle rectangle = new Rectangle(50, 50, 33, 75);
         public Form1()
         {
             InitializeComponent();
             pictureBox1.MouseDown += PictureBox1_MouseDown;
             pictureBox1.MouseMove += PictureBox1_MouseMove;
             pictureBox1.MouseUp += PictureBox1_MouseUp;
+            Timer timer1 = new Timer { Interval = 1, Enabled = true };
+            timer1.Tick += new EventHandler(OnTimerEvent);
         }
 
-        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        private void OnTimerEvent(object sender, EventArgs e)
         {
-            rectangleClicked = false;
-            circleClicked = false;
-            squareClicked = false;
-            if (rectangleClicked)
+            rectangle.Y = rectangle.Y + speed;
+            if ((rectangle.Location.X < circle.X + circle.Width) && (rectangle.Location.X > circle.X - circle.Width) &&
+                (rectangle.Location.Y < circle.Y + circle.Height) && (rectangle.Location.Y > circle.Y - circle.Width))
             {
-                LastClicked = 1;
-                X = rectangle.X;
-                Y = rectangle.Y;
-            }
-            else if (circleClicked)
-            {
-                LastClicked = 2;
-                X = circle.X;
-                Y = circle.Y;
-            }
-            else
-            {
-                LastClicked = 3;
-                X = square.X;
-                Y = square.Y;
-            }
-            if ((((label2.Location.X < rectangle.X + rectangle.Width) && (label2.Location.X > rectangle.X) &&
-                (label2.Location.Y < rectangle.Y + rectangle.Height) && (label2.Location.Y > rectangle.Y)) ||
-                ((label2.Location.X < circle.X + circle.Width) && (label2.Location.X > circle.X) &&
-                (label2.Location.Y < circle.Y + circle.Height) && (label2.Location.Y > circle.Y)) ||
-                ((label2.Location.X < square.X + square.Width) && (label2.Location.X > square.X) &&
-                (label2.Location.Y < square.Y + square.Height) && (label2.Location.Y > square.Y)))
-                &&
-                swapped)
-            {
-                swapped = false;
-                if (rectangleClicked)
+                score = score + 10;
+                if(score % 100 == 0)
                 {
-                    if (LastClicked == 2)
-                    {
-                    }
-                    else if (LastClicked == 3)
-                    {
-                    }
+                    speed = speed + 2;
                 }
-                else if (circleClicked)
-                {
-                    if (LastClicked == 1)
-                    {
-                    }
-                    else if (LastClicked == 3)
-                    {
-                    }
-                }
-                else if (squareClicked)
-                {
-                    if (LastClicked == 1)
-                    {
-                    }
-                    else if (LastClicked == 2)
-                    {
-                        circle.X = square.X;
-                        circle.Y = square.Y;
-
-                        square.X = X;
-                        square.Y = Y;
-                    }
-                }
-            }
-        }
-
-        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (rectangleClicked)
-            {
-                rectangle.X = e.X - x1;
-                rectangle.Y = e.Y - y1;
-                if (((label1.Location.X < rectangle.X + rectangle.Width) && (label1.Location.X > rectangle.X) &&
-                    (label1.Location.Y < rectangle.Y + rectangle.Height) && (label1.Location.Y > rectangle.Y)))
-                {
-                    label1.Text = "Желтый";
-                    label2.Text = "Прямоугольник";
-                    label3.Text = "Желтый прямоугольник";
-                }
-            }
-            else if (circleClicked)
-            {
-                circle.X = e.X - x2;
-                circle.Y = e.Y - y2;
-                if (((label1.Location.X < circle.X + circle.Width) && (label1.Location.X > circle.X) &&
-                    (label1.Location.Y < circle.Y + circle.Height) && (label1.Location.Y > circle.Y)))
-                {
-                    label1.Text = "Красный";
-                    label2.Text = "Круг";
-                    label3.Text = "Красный круг";
-                }
-            }
-            else if (squareClicked)
-            {
-                square.X = e.X - x3;
-                square.Y = e.Y - y3;
-                if (((label1.Location.X < square.X + square.Width) && (label1.Location.X > square.X) && 
-                    (label1.Location.Y < square.Y + square.Height) && (label1.Location.Y > square.Y)))
-                {
-                    label1.Text = "Синий";
-                    label2.Text = "Квадрат";
-                    label3.Text = "Синий квадрат";
-                }
+                label1.Text = score.ToString();
+                rectangle.Y = 50;
             }
             pictureBox1.Invalidate();
         }
 
-        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
-            swapped = true;
-            if ((e.X < rectangle.X + rectangle.Width) && (e.X > rectangle.X))
+            if (((label1.Location.X < rectangle.X + rectangle.Width) && (label1.Location.X > rectangle.X) &&
+                (label1.Location.Y < rectangle.Y + rectangle.Height) && (label1.Location.Y > rectangle.Y)))
             {
-                if ((e.Y < rectangle.Y + rectangle.Height) && (e.Y > rectangle.Y))
+                if (circleClicked)
                 {
-                    rectangleClicked = true;
-
-                    x1 = e.X - rectangle.X;
-                    y1 = e.Y - rectangle.Y;
+                    if (LastClicked == 2)
+                    {
+                        circle.X = rectangle.X;
+                        circle.Y = rectangle.Y;
+                    }
+                    else if (LastClicked == 3)
+                    {
+                        square.X = rectangle.X;
+                        square.Y = rectangle.Y;
+                    }
                 }
             }
+            if (circleClicked)
+            {
+            }
+            circleClicked = false;
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (circleClicked)
+            {
+                if (e.X > 50 && e.X < 350)
+                {
+                    circle.X = e.X - x2;
+                }
+                if (((label1.Location.X < circle.X + circle.Width) && (label1.Location.X > circle.X) &&
+                    (label1.Location.Y < circle.Y + circle.Height) && (label1.Location.Y > circle.Y)))
+                {
+                }
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
             if ((e.X < circle.X + circle.Width) && (e.X > circle.X))
             {
                 if ((e.Y < circle.Y + circle.Height) && (e.Y > circle.Y))
                 {
                     circleClicked = true;
-
                     x2 = e.X - circle.X;
-                    y2 = e.Y - circle.Y;
-                }
-            }
-            if ((e.X < square.X + square.Width) && (e.X > square.X))
-            {
-                if ((e.Y < square.Y + square.Height) && (e.Y > square.Y))
-                {
-                    squareClicked = true;
-
-                    x3 = e.X - square.X;
-                    y3 = e.Y - square.Y;
                 }
             }
         }
